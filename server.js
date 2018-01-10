c06bb3fc70a294b9bff601b52a6424688a3bf129
Junhson <jjeanba2@binghamton.edu>
2018-01-10 00:35:05 -0500

    commented out entire server code

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-//this is the server code for my web site
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end();
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server Running at http://${hostname}:${port}`);
-});
+// //this is the server code for my web site
+// const http = require('http');
+//
+// const hostname = '127.0.0.1';
+// const port = 3000;
+//
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end();
+// });
+//
+// server.listen(port, hostname, () => {
+//   console.log(`Server Running at http://${hostname}:${port}`);
+// });
