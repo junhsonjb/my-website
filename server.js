e7dc65cdba19a8d96ffd22235a12f3976669cc35
Junhson <jjeanba2@binghamton.edu>
2018-01-10 11:03:19 -0500

    got the server to show my basic site, emojis don't work yet

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 //this is the server code for my web site
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end("Hello World from Junhson");
-});
+fs.readFile('index.html', (err, html) => {
 
-server.listen(port, hostname, () => {
-  console.log(`Server Running at http://${hostname}:${port}`);
+  if (err) {
+    throw err;
+  }
+
+  //if no error then run the server code
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write(html);
+    res.end();
+  });
+
+  server.listen(port, hostname, () => {
+    console.log(`Server Running at http://${hostname}:${port}`);
+  });
+
 });
