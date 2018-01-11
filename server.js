6c1d1a1407cef03e82f0481ad707acc03bd0141b
Junhson <jjeanba2@binghamton.edu>
2018-01-11 17:11:35 -0500

    organized server code

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
-//this is the server code for my web site
-const http = require('http');
-const fs = require('fs');
+/* Dependecies */
+var http = require('http');
+var fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var hostname = '127.0.0.1'; //just the place to host it at
+var port = 3000; //the port
 
-fs.readFile('index.html', (err, html) => {
+/* Function to call when user requests a page that doesn't exist */
+function send404(response) {
+  response.writeHead(404, {"Content-Type" : "text/plain"});
+  response.write("Status Code 404: File Not Found");
+  response.end();
+}
 
-  if (err) {
-    throw err;
+/* when called, this function will serve up the user's requests or send a 404 msg */
+function runner(request, response) {
+
+  /* Server Code here */
+  if (request.method == 'GET' && request.url == "/") {
+    response.writeHead(200, {"Content-Type" : "text/html"});
+    fs.createReadStream("./index.html").pipe(response);
+  } else {
+    send404(response);
   }
 
-  //if no error then run the server code
-  const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.write(html);
-    res.end();
-  });
+}
 
-  server.listen(port, hostname, () => {
-    console.log(`Server Running at http://${hostname}:${port}`);
-  });
+/* call the runner function to create a server */
+var server = http.createServer(runner);
 
+/* set the server to listen at the defined port and hostname */
+server.listen(port, hostname, () => {
+  console.log(`Server Running at http://${hostname}:${port}`);
 });
