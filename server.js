aee923633d3a6f477740a7d5e5e44967a552906d
Junhson <jjeanba2@binghamton.edu>
2018-01-17 17:46:17 -0500

    used express to get the resume up, going to try emojis and css too

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.static('public'));
 /* Just some constant values contained here */
 const hostname = '127.0.0.1'; //just the place to host it at
 const port = 3000; //the port
+const success = `Server Running at http://${hostname}:${port}`
 
 /* Function to call when user requests a page that doesn't exist */
 function send404(response) {
@@ -18,24 +19,41 @@ function send404(response) {
   response.end();
 }
 
+function resumePDF(request, response) {
+  const resume = "./files/resume.pdf";
+  fs.readFile(resume, function(err, data) {
+    response.contentType("application/pdf");
+    response.send(data);
+  });
+}
+
 /* when called, this function will serve up the user's requests or send a 404 msg */
 function runner(request, response) {
 
   /* Server Code here */
-  if (request.method == 'GET' && request.url == "/") {
-    response.writeHead(200, {"Content-Type" : "text/html"});
-    fs.createReadStream("./index.html").pipe(response);
-  } else {
-    send404(response);
-  }
+  //old server code (WITHOUT EXPRESS)
+  // response.writeHead(200, {"Content-Type" : "text/html"});
+  // fs.createReadStream("./index.html").pipe(response);
+
+  const index = "./index.html";
+  fs.readFile(index, function(err, data) {
+    response.contentType("text/html");
+    response.send(data);
+  });
+
 
 }
 
-/* call the runner function on a server variable to create a server */
-var server = http.createServer(runner);
+// Original Running Code (WITHOUT EXPRESS)
+// /* call the runner function on a server variable to create a server */
+// var server = http.createServer(runner);
+//
+// /*  tell the server variable to listen at either the defined port or the
+//     one defined by the environment in which the code is running */
+// server.listen(process.env.PORT || port, () => {
+//   console.log(`Server Running at http://${hostname}:${port}`);
+// });
 
-/*  tell the server variable to listen at either the defined port or the
-    one defined by the environment in which the code is running */
-server.listen(process.env.PORT || port, () => {
-  console.log(`Server Running at http://${hostname}:${port}`);
-});
+app.get('/', runner);
+app.get('/resume', resumePDF);
+app.listen(process.env.PORT || port, () => console.log(success));
