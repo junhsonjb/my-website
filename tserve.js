233ace07d1d816ca4e89933b9a670dbe7b7a7124
Junhson <jjeanba2@binghamton.edu>
2018-01-17 13:39:07 -0500

    still learning express, have to configure the app generator

--- tserve.js
+++ tserve.js
@@ -13,6 +13,13 @@ const success = "app running on port " + port + "!";
 /* declare my express app */
 const app = express();
 
+/* General 404 method */
+function send404(response) {
+  response.writeHead(404, {"Content-Type" : "text/plain"});
+  response.write("Status Code 404: File Not Found");
+  response.end();
+}
+
 app.get('/', (request, response) => response.send("Hello, World") );
 
 app.listen(port, () => console.log(success));
