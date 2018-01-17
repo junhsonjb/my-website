8d5690896833c2532f3c06ca71762c9497c5ec58
Junhson <jjeanba2@binghamton.edu>
2018-01-17 17:29:39 -0500

    added method to serve pdf with express

--- tserve.js
+++ tserve.js
@@ -13,6 +13,8 @@ const success = "app running on port " + port + "!";
 /* declare my express app */
 const app = express();
 
+app.use(express.static('public'));
+
 /* General 404 method */
 function send404(response) {
   response.writeHead(404, {"Content-Type" : "text/plain"});
@@ -20,6 +22,19 @@ function send404(response) {
   response.end();
 }
 
-app.get('/', (request, response) => response.send("Hello, World") );
+/* function to run on the get method for '/' route */
+function resumePDF(request, response) {
+  const resume = "./files/resume.pdf";
+  fs.readFile(resume, function(err, data) {
+    response.contentType("application/pdf");
+    response.send(data);
+  });
+}
+
+function runner(request, response) {
+  response.send("This is cool");
+}
+
+app.get('/', runner);
 
 app.listen(port, () => console.log(success));
