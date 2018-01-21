3b0e4b3eb8758457b00168fb475adba1c9f09759
Junhson <jjeanba2@binghamton.edu>
2018-01-20 23:59:09 -0500

    since its the dev branch, I copied contents of tserve.js into server.js. I also got emojis working

--- server.js
+++ server.js
@@ -1,79 +1,52 @@
-/* Dependecies */
+/* This file will hold test server code */
+
+/* requires go here */
+const express = require('express');
 const http = require('http');
 const fs = require('fs');
-const express = require('express');
 
-/* 'app' (express variable) declaration and its middleware */
+/* some helpful constants */
+const hostname = '127.0.0.1';
+const port = 3000;
+const success = "app running on port " + port + "!";
+
+/* declare my express app */
 const app = express();
+
+/* tell express that we want to use static files in the 'public' folder */
 app.use(express.static('public'));
 
-/* Just some constant values contained here */
-const hostname = '127.0.0.1'; //just the place to host it at
-const port = 3000; //the port
-const success = `Server Running at http://${hostname}:${port}`
-
-/* Function to call when user requests a page that doesn't exist */
+/* General 404 method */
 function send404(response) {
   response.writeHead(404, {"Content-Type" : "text/plain"});
   response.write("Status Code 404: File Not Found");
   response.end();
 }
 
-function resumePDF(request, response) {
-  const resume = "./files/resume.pdf";
+/* function to run on the get method for '/resume' route */
+//...
+
+/* this is the method that handles responses to/for the home page */
+function runner(request, response) {
+  //response.writeHead(200, {"Content-Type" : "text/html"});
+  /* const index = "/index.html"; */
+  const emoji = "./public/emoji.html"; //the homepage with emojis in it
+  fs.readFile(emoji, function(err, data) {
+    response.contentType("text/html");
+    response.send(data);
+  });
+}
+
+function writeresume(request, response) {
+  const resume = "./public/files/resume.pdf";
   fs.readFile(resume, function(err, data) {
     response.contentType("application/pdf");
     response.send(data);
   });
 }
 
-/* when called, this function will serve up the user's requests or send a 404 msg */
-function runner(request, response) {
-
-  /* Server Code here */
-  //old server code (WITHOUT EXPRESS)
-  // response.writeHead(200, {"Content-Type" : "text/html"});
-  // fs.createReadStream("./index.html").pipe(response);
-
-  const index = "./index.html";
-  fs.readFile(index, function(err, data) {
-    response.contentType("text/html");
-    response.send(data);
-  });
-
-
-}
-
-/* Trying to see if I can use express to serve up an html file with emojis */
-// function emojiRunner(request, response) {
-//
-//   /* Server Code here */
-//   //old server code (WITHOUT EXPRESS)
-//   // response.writeHead(200, {"Content-Type" : "text/html"});
-//   // fs.createReadStream("./index.html").pipe(response);
-//
-//   const index = "./introduction-emoji.html";
-//   fs.readFile(index, function(err, data) {
-//     response.contentType("text/html");
-//     response.send(data);
-//   });
-//
-//   // response.send("Cool, dude!");
-//
-//
-// }
-
-// Original Running Code (WITHOUT EXPRESS)
-// /* call the runner function on a server variable to create a server */
-// var server = http.createServer(runner);
-//
-// /*  tell the server variable to listen at either the defined port or the
-//     one defined by the environment in which the code is running */
-// server.listen(process.env.PORT || port, () => {
-//   console.log(`Server Running at http://${hostname}:${port}`);
-// });
-
 app.get('/', runner);
-// app.get('/emoji', emojiRunner); //doesn't work
-app.get('/resume', resumePDF);
-app.listen(process.env.PORT || port, () => console.log(success));
+
+app.get('/resume', writeresume);
+
+app.listen(port, () => console.log(success));
