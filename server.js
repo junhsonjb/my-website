5ae19d0b322edc26e9e5c469c664d3369347064f
Junhson <jjeanba2@binghamton.edu>
2018-01-21 01:05:33 -0500

    trying to make it run on heroku, edited server code and package.json

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-/* This file will hold test server code */
-
 /* requires go here */
 const express = require('express');
 const http = require('http');
 const fs = require('fs');
-
-/* some helpful constants */
-const hostname = '127.0.0.1';
-const port = 3000;
-const success = "app running on port " + port + "!";
 
 /* declare my express app */
 const app = express();
 
 /* tell express that we want to use static files in the 'public' folder */
 app.use(express.static('public'));
+
+/* some helpful constants */
+const hostname = '127.0.0.1';
+const port = 3000;
+const success = "app running on port " + port + "!";
 
 /* General 404 method */
 function send404(response) {
@@ -46,7 +44,6 @@ function writeresume(request, response) {
 }
 
 app.get('/', runner);
-
 app.get('/resume', writeresume);
 
 app.listen(process.env.PORT || port, () => console.log(success));
